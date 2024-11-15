0bb3ce4ea5e54c4f318753e58fb7b9265278e50d
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2024-11-15 17:24:45 +0300

    Lesson 3: Component Styling

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
  @Component({
     selector:'movies',
     templateUrl:'movies.component.html',
+    styleUrls:['./movies.component.css']
 })
 export class MoviesComponent{
     
